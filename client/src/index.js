23440775e2fe68f14fa542ab014ce378cabe971f
krollkim <kimkroll2000@gmail.com>
2024-12-14 03:44:25 +0700

    Apply .gitignore rules

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,9 @@ import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
 
+console.log('React instance path:', require.resolve('react'));
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
